02edc8544dfd5e56ea6fceddf0880a37053ea05c
ITPS <ITPS@gmail.com>
2022-04-11 12:53:29 +0900

    2022년 4월 11일 수업내용 데이터베이스 활용해서 로그인 시스템 만들기

---
+++ src/main/java/repository/user/User.java
@@ -0,0 +1,22 @@
+package repository.user;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+	private int user_code;
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+	private LocalDateTime create_date;
+	private LocalDateTime update_date;
+}
